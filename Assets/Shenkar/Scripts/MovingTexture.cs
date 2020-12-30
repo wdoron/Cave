8799f504c36fdec28335c05c6a390bfb101cedbe
Doron <weiss.doron@gmail.com>
2020-12-30 14:32:17 +0200

    Version2 of escape room

---
+++ Assets/Shenkar/Scripts/MovingTexture.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MovingTexture : MonoBehaviour
+{
+    private Renderer rndr;
+    public Vector2 offsetPerSec;
+    public string textureId = "_MainTex";
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rndr = GetComponent<Renderer>();
+    }
+
+    //private void OnDrawGizmosSelected() {
+    //    if (rndr == null) Awake();
+    //    Update();
+    //} 
+
+    void Update()
+    {
+
+        var offset = rndr.sharedMaterial.GetTextureOffset(textureId);
+        rndr.sharedMaterial.SetTextureOffset(textureId, offset + offsetPerSec * Time.deltaTime);
+    }
+}
